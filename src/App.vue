d8cc6fb7cbeecb65a8462f7ec9206b46ff41cacd
70X <powe.fox@gmail.com>
2025-08-12 10:27:56 +0200

    feat: add vue router v4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
+const activeClassColor = "text-green-500"
 </script>
 
 <template>
-  <div>
-		<h1 class="text-red-500">Vue 3 + tailwindcss</h1>
-  </div>
+	<div class="flex flex-col h-dvh w-dvw p-5">
+		<div class="flex w-100 justify-between">
+			<h1 class="text-purple-600">Logo: Vue 3 + Router + tailwindcss</h1>
+			<p>
+				<strong>Current route path:</strong> {{ $route.fullPath }}
+			</p>
+			<nav class="flex gap-2">
+				<RouterLink to="/" :active-class="activeClassColor">Home</RouterLink>
+				<RouterLink to="/about" :active-class="activeClassColor">About</RouterLink>
+			</nav>
+		</div>
+		<main>
+			<RouterView />
+		</main>
+	</div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
